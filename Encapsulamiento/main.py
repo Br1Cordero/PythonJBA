8c2406c855c23ddcb14ffa9c71c2b7005d7c895b
Br1Cordero <bcorderos.itsjba@gmail.com>
2023-02-10 23:26:43 -0500

    006

---
+++ Encapsulamiento/main.py
@@ -0,0 +1,9 @@
+import Clases as c
+
+compu =  c.Computadora(marca = 'Dell', procesador = 'Intel Core 7', memoria = 16, almacenamiento =500 , sistema_operativo = 'Windoxs' )
+
+compu.apagar()
+compu.actualizar_sistema_operativo()
+compu.ver_especificaciones()
+compu.instalar_programa()
+compu.desinstalar_programa()
